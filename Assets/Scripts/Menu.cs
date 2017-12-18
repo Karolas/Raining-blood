0b79025a38e875b0e0624aa20d72cccc7c99ed16
Karolas <karolisb694@gmail.com>
2017-12-18 05:39:43 +0200

    UI changes

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,13 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+	public GameObject mainMenu;
+	public GameObject playMenu;
+
 	public void Play()
     {
-        SceneManager.LoadScene("Game");
+		mainMenu.SetActive(false);
+		playMenu.SetActive(true);
+    }
+	
+	public void HowToPlay()
+    {
+        
+    }
+	
+	public void Options()
+    {
+        
     } 
 
     public void Quit()
     {
         Application.Quit();
     }
+	
+	public void PlayToMainMenu()
+    {
+        mainMenu.SetActive(true);
+		playMenu.SetActive(false);
+    }
+	
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("Game");
+	}
 }
